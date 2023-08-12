43a0592c50c654731853da29a44dc8de6d9f42db
zmy <3588617325@qq.com>
2023-08-12 17:48:20 +0800

    小米轮播图-代码优化

--- mi/script/carousel.js
+++ mi/script/carousel.js
@@ -13,10 +13,8 @@ window.addEventListener("load", function () {
   var imgsArray = this.document.querySelectorAll(
     "div.carousel>div.container>img"
   );
-  // 定义一个全局变量，来表示当前小圆点的位置/数字
-  var currentDotNumber = 0;
-  // 定义一个全局变量，来表示当前图片的位置/数字
-  var currentImgNumber = 0;
+  // 定义一个全局变量，来表示当前小圆点和图片的位置/数字
+  var currentNumber = 0;
   // 函数节流，上锁
   var lock = false;
   // 定义轮播图自动播放
@@ -28,11 +26,9 @@ window.addEventListener("load", function () {
       return;
     }
     lock = true;
-    // 小圆点高亮
-    dotHighLight(--currentDotNumber);
-    // 图片显示
-    imgShow(--currentImgNumber);
-    // 延时1s
+    // 小圆点高亮，图片显示
+    dotHighLightAndImgShow(--currentNumber);
+    // 节流
     setTimeout(() => {
       lock = false;
     }, 1000);
@@ -43,55 +39,36 @@ window.addEventListener("load", function () {
       return;
     }
     lock = true;
-    // 小圆点高亮
-    dotHighLight(++currentDotNumber);
-    // 图片显示
-    imgShow(++currentImgNumber);
-    // 延时1s
+    // 小圆点高亮，图片显示
+    dotHighLightAndImgShow(++currentNumber);
+    // 节流
     setTimeout(() => {
       lock = false;
     }, 1000);
   };
-  // 定义函数
-  // 小圆点高亮
-  function dotHighLight(index) {
+  /**
+   * 小圆点高亮和图片显示，函数二合一
+   * @param {number} index 索引
+   */
+  function dotHighLightAndImgShow(index) {
     // 判断索引是否正常
     if (index === -1) {
       // 两步合为一步
-      // 第一步，更新currentDotNumber的值为4
+      // 第一步，更新currentNumber的值为4
       // 第二步，更新index的值为4
-      index = currentDotNumber = 4;
+      index = currentNumber = 4;
     }
     if (index === 5) {
-      index = currentDotNumber = 0;
+      index = currentNumber = 0;
     }
-    // 先让所有的小圆点熄灭
+    // 先让所有的小圆点熄灭,图片隐藏
     for (var i = 0; i < dotsArray.length; i++) {
-      // 让所有的小圆点熄灭
+      // 让所有的小圆点熄灭,图片隐藏
       dotsArray[i].classList.remove("active");
-    }
-    // 让指定索引的小圆点高亮
-    dotsArray[index].classList.add("active");
-  }
-  // 定义函数
-  // 图片显示
-  function imgShow(index) {
-    // 判断索引是否正常
-    if (index === -1) {
-      // 两步合为一步
-      // 第一步，更新currentImgNumber的值为4
-      // 第二步，更新index的值为4
-      index = currentImgNumber = 4;
-    }
-    if (index === 5) {
-      index = currentImgNumber = 0;
-    }
-    // 先让所有的图片熄灭
-    for (var i = 0; i < imgsArray.length; i++) {
-      // 让所有的图片熄灭
       imgsArray[i].classList.remove("img-active");
     }
-    // 让指定索引的图片显示
+    // 让指定索引的小圆点高亮，图片显示
+    dotsArray[index].classList.add("active");
     imgsArray[index].classList.add("img-active");
   }
   //小圆点和图片联动
@@ -102,11 +79,9 @@ window.addEventListener("load", function () {
       // 获取小圆点当前的索引
       var currentIndex = this.dataset.index;
       // 按钮高亮，显示图片
-      dotHighLight((currentDotNumber = currentIndex));
-      imgShow((currentImgNumber = currentIndex));
+      dotHighLightAndImgShow((currentNumber = currentIndex));
     };
   });
-
   // 定义函数，自动播放
   function autoPlay() {
     // 只要添加新的定时器，就要把之前的定时器关掉，防止累加，速度过快
