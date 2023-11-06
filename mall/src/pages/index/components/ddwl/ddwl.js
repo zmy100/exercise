d4433a086998af785dccf8b7b1105cd45cce6210
zmy100 <zhangmengying0703@hotmail.com>
2023-11-06 21:49:18 +0800

    local_fun.json接口完成

--- mall/src/pages/index/components/ddwl/ddwl.js
+++ mall/src/pages/index/components/ddwl/ddwl.js
@@ -1,1 +1,15 @@
 import "./ddwl.css";
+import axios from "axios";
+import render from "./ddwl.art";
+
+// 调用接口
+axios
+  .get("../src/api/ajax-data/local_fun.json")
+  .then(function (response) {
+    document.querySelector("section.ddwl").innerHTML = render(
+      response.data.data
+    );
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
