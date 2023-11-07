1d120ab3a6fb2b33118cc83ccd790b7b1cd96d9d
zmy100 <zhangmengying0703@hotmail.com>
2023-11-07 15:05:52 +0800

    special_subject.json接口完成

--- mall/src/pages/index/components/zsj/zsjsdlx.js
+++ mall/src/pages/index/components/zsj/zsjsdlx.js
@@ -1,1 +1,15 @@
 import "./zsjsdlx.css";
+import axios from "axios";
+import render from "./zsjsdlx.art";
+
+// 调用接口
+axios
+  .get("../src/api/ajax-data/special_subject.json")
+  .then(function (response) {
+    document.querySelector("section.zsjsdlx").innerHTML = render(
+      response.data.data
+    );
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
