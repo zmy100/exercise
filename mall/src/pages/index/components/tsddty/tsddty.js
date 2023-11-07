280818fa68ad4c23ba00fcb8ea6cfb588217879d
zmy100 <zhangmengying0703@hotmail.com>
2023-11-07 14:52:38 +0800

    local_exp.json接口完成

--- mall/src/pages/index/components/tsddty/tsddty.js
+++ mall/src/pages/index/components/tsddty/tsddty.js
@@ -1,1 +1,15 @@
 import "./tsddty.css";
+import axios from "axios";
+import render from "./tsddty.art";
+
+// 调用接口
+axios
+  .get("../src/api/ajax-data/local_exp.json")
+  .then(function (response) {
+    document.querySelector("section.tsddty").innerHTML = render({
+      items: response.data.data,
+    });
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
