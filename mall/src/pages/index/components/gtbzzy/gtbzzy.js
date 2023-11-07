41128e2a7aad5bc42587a52309687bd4f68a749d
zmy100 <zhangmengying0703@hotmail.com>
2023-11-07 15:13:16 +0800

    group_tour.json接口完成

--- mall/src/pages/index/components/gtbzzy/gtbzzy.js
+++ mall/src/pages/index/components/gtbzzy/gtbzzy.js
@@ -1,1 +1,15 @@
 import "./gtbzzy.css";
+import axios from "axios";
+import render from "./gtbzzy.art";
+
+// 调用接口
+axios
+  .get("../src/api/ajax-data/group_tour.json")
+  .then(function (response) {
+    document.querySelector("section.gtbzzy").innerHTML = render(
+      response.data.data
+    );
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
