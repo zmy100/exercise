6d99c59cfa357fb6aa06a805d329e8ffd3b0b8db
zmy100 <zhangmengying0703@hotmail.com>
2023-11-07 08:57:52 +0800

    self_guided_tour.json接口完成

--- mall/src/pages/index/components/jjzyx/jjzyx.js
+++ mall/src/pages/index/components/jjzyx/jjzyx.js
@@ -1,1 +1,15 @@
 import "./jjzyx.css";
+import axios from "axios";
+import render from "./jjzyx.art";
+
+// 调用接口
+axios
+  .get("../src/api/ajax-data/self_guided_tour.json")
+  .then(function (response) {
+    document.querySelector("section.jjzyx").innerHTML = render(
+      response.data.data
+    );
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
