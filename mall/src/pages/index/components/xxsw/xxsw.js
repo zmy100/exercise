8457bcb0669775fc33ee5bce667eefa5dde11313
zmy100 <zhangmengying0703@hotmail.com>
2023-11-07 08:46:48 +0800

    seckill.json接口完成

--- mall/src/pages/index/components/xxsw/xxsw.js
+++ mall/src/pages/index/components/xxsw/xxsw.js
@@ -1,1 +1,15 @@
 import "./xxsw.css";
+import axios from "axios";
+import render from "./xxsw.art";
+
+// 调用接口
+axios
+  .get("../src/api/ajax-data/seckill.json")
+  .then(function (response) {
+    document.querySelector("section.xxsw").innerHTML = render({
+      items: response.data.data,
+    });
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
