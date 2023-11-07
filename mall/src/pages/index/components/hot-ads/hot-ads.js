1d5068f816666fe01e26946a6de715f1e1744136
zmy100 <zhangmengying0703@hotmail.com>
2023-11-07 08:42:22 +0800

    ad.json接口完成

--- mall/src/pages/index/components/hot-ads/hot-ads.js
+++ mall/src/pages/index/components/hot-ads/hot-ads.js
@@ -1,1 +1,15 @@
 import "./hot-ads.css";
+import axios from "axios";
+import render from "./hot-ads.art";
+
+// 调用接口
+axios
+  .get("../src/api/ajax-data/ad.json")
+  .then(function (response) {
+    document.querySelector("section.hot-ads").innerHTML = render({
+      items: response.data.data,
+    });
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
